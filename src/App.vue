d8b2897006643e03d017a21fc3d31fc77346e36a
Pavel <yashchenkodev@gmail.com>
2026-04-01 23:54:09 +0300

    feat: add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <AppHeader />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
+
+<style>
+.container {
+  max-width: 1280px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface IMenuItem {
+  label: string
+  icon: string
+  path: string
+}
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Авторизація',
+    icon: 'pi pi-user',
+    path: '/auth',
+  },
+  {
+    label: 'Додати',
+    icon: 'pi pi-plus',
+    path: '/',
+  },
+  {
+    label: 'Список співбесід',
+    icon: 'pi pi-list',
+    path: '/list',
+  },
+  {
+    label: 'Статистика',
+    icon: 'pi pi-chart-pie',
+    path: '/statistic',
+  },
+])
+</script>
+
+<template>
+  <app-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+      <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{ item.label }}</span>
+      </router-link>
+    </template>
+  </app-menubar>
+</template>
+
+<style scoped>
+.menu {
+  margin: 30px 0;
+}
+
+.menu-exit {
+  cursor: pointer;
+}
+</style>
